b922f4b005b73f3e8b6e576757670f6c829518d6
sunnychau2002 <136631318+sunnychau2002@users.noreply.github.com>
2023-06-26 21:55:05 +0700

    Optimize deployment process

---
+++ xylophone_apple_zebra.vb
@@ -0,0 +1,16 @@
+Imports System
+
+Public Class MainClass 
+	
+	'This program prints the message "Spread the Word"
+	Public Shared Sub Main() 
+
+		Dim line As Integer = 0
+		Do While line < 2000
+
+			Console.WriteLine("Spread the Word")
+			line = line + 1	
+
+		Loop
+	End Sub
+End Class
