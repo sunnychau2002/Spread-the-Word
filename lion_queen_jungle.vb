d4b12681f0ca12152682ac59a8d656a251a746a6
sunnychau2002 <136631318+sunnychau2002@users.noreply.github.com>
2023-07-17 19:47:15 +0700

    Enhance password security

---
+++ lion_queen_jungle.vb
@@ -0,0 +1,15 @@
+Public Class SpreadtheWord
+
+Private Const message As String = "Spread the Word!"
+
+Public Sub Main()
+
+For index As Integer = 1 To 2000
+    Console.WriteLine(message)
+Next
+
+Console.ReadKey()
+
+End Sub
+
+End Class
